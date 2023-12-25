db6e7023cf8e688d39c4487ba261736ae05a836b
jhonatanmizu <natanjesuss20@gmail.com>
2023-12-25 13:54:14 -0300

    :lipstick: update container styles

--- src/app/common/components/Container.tsx
+++ src/app/common/components/Container.tsx
@@ -1,7 +1,11 @@
 import { PropsWithChildren } from "react";
 
 const Container = ({ children }: PropsWithChildren) => {
-  return <div className="max-w-screen-xl m-auto">{children}</div>;
+  return (
+    <div className="max-w-screen-xl m-auto bg-primary min-h-full">
+      {children}
+    </div>
+  );
 };
 
 export default Container;
